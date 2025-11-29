7d8aa54087f21a7c63758f3b224e7995b2ec2f11
Edwin Bravo <edwinbravo2121@gmail.com>
2025-11-28 22:38:40 -0500

    volver a lo mismo

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use("/api/ventas", require("./routes/ventaRouter"));         // Ventas
 app.use("/api/dashboard", require("./routes/dashboardRouter"));  // Dashboard
 app.use("/api/profile", require("./routes/profileRouter"));      // Perfil
 app.use("/api/productos", require("./routes/productoRouter"));   // Productos
-app.use("/api/carrito", require("./routes/carritoRouter"));      // 👈 NUEVO: Carrito temporal
+
 
 // =====================
 // Ruta raíz (ping del servidor)
